73583b5c2f78545be4f031614d24038d2dfca75d
Pinshuo Ye <yepinshuo@berkeley.edu>
2019-04-30 22:04:34 -0700

    Changes

--- solver.py
+++ solver.py
@@ -14,8 +14,8 @@ def solve(client):
     mst=nx.minimum_spanning_tree(client.G)
     pathLength = {}
     for vertex in mst.nodes:
-		currLen = nx.shortest_path_length(mst,client.h,vertex)
-		pathLength[vertex] = currLen
+        currLen = nx.shortest_path_length(mst,client.h,vertex)
+        pathLength[vertex] = currLen
     all_students = list(range(1, client.students + 1))
     non_home = list(range(1, client.home)) + list(range(client.home + 1, client.v + 1))
     
@@ -38,9 +38,18 @@ def solve(client):
     		ScoutResults[vertex] = False
 
 
-   	while client.bot_count[client.h] != client.l:
-   		furBot = findFurthestBot(client,mst,pathLength)
-   		remoteBot(client,mst,furbot,pathLength)
+    furBot = findFurthestBot(client,mst,pathLength)
+    print(furBot)
+    #remoteBot(client,mst,furBot,pathLength)
+    #for vertex in non_home:
+    #    if ScoutResults[vertex]:
+    #        remoteBot(client, mst, vertex, pathLength)
+
+
+   	#while client.bot_count[client.h] != client.l:
+    #while i in range(10):
+   	#	furBot = findFurthestBot(client,mst,pathLength)
+   	#	remoteBot(client,mst,furBot,pathLength)
 
     client.end()
 
@@ -49,20 +58,21 @@ def knownBotsEqualToTotal(client):
 	return len(client.bot_locations()) == client.l
 
 
-def findFurthestBot(client,mst,pathLength):
+def findFurthestBot(client, mst, pathLength):
 	# unique vertex indices of all bot 
 	bot_loc = set(client.bot_locations)
 	max_Len = 0
 	max_bot = None
 	for vertex in mst.nodes:
-		if vertex in bot_loc & pathLength[vertex] > max_Len:
-			max_Len = currLen
+        if vertex in bot_loc:
+            print("vertex is in botlOC")
+		if (vertex in bot_loc) and (pathLength[vertex] > max_Len):
+			max_Len = pathLength[vertex] 
 			max_bot = vertex
-
 
 	return max_bot		
 
-def remoteBot(client,mst, bot, pathLength):
+def remoteBot(client, mst, bot, pathLength):
 	neighbors = mst.neighbors(bot)
 	minLen = 0
 	minNeighbor = None
@@ -70,7 +80,7 @@ def remoteBot(client,mst, bot, pathLength):
 		if minLen > pathLength[neighbor]:
 			minLen = pathLength[neighbor]
 			minNeighbor = neighbor
-	client.remote(bot,minNeighbor)
+	client.remote(bot, minNeighbor)
 
 
 		
